63f733c72adc693e38d59e82d864029c57435a12
smioner <120722161+smioner@users.noreply.github.com>
2023-03-10 13:09:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-def Verifica_10_3_Gruppo_1(nome, cognome):
+def Verifica_10_3_Gruppo_3(nome, cognome):
     """Restituisce una stringa di saluto con nome e cognome"""
     return f"Ciao {nome} {cognome}, piacere di conoscerti!"
   
   nome_utente = input("Inserisci il tuo nome: ")
 cognome_utente = input("Inserisci il tuo cognome: ")
-saluto = Verifica_10_3_Gruppo_1(cognome_utente,nome_utente)
+saluto = Verifica_10_3_Gruppo_3(cognome_utente,nome_utente)
 print(saluto)
 
-Verifica_10_3_Gruppo_1()
+Verifica_10_3_Gruppo_3()
