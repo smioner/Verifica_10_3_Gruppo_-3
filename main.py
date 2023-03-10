37daa01c666990d0106b1f8f43d77d31b5157f15
smioner <120722161+smioner@users.noreply.github.com>
2023-03-10 13:09:08 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-from saluto_cognome_Gruppo3 import saluto_cognome_Gruppo3
+def Verifica_10_3_Gruppo_1(nome, cognome):
+    """Restituisce una stringa di saluto con nome e cognome"""
+    return f"Ciao {nome} {cognome}, piacere di conoscerti!"
+  
+  nome_utente = input("Inserisci il tuo nome: ")
+cognome_utente = input("Inserisci il tuo cognome: ")
+saluto = Verifica_10_3_Gruppo_1(cognome_utente,nome_utente)
+print(saluto)
 
-saluto_cognome_Gruppo3()
+Verifica_10_3_Gruppo_1()
